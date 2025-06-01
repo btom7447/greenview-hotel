7aeac82aa03b329fb1fd23d6716e30c927e8e8d2
btom7447 <tombenjamin7447@gmail.com>
2025-06-01 01:47:36 +0100

    optimization v.2.1

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -35,7 +35,7 @@ const Breadcrumb = ({ title, subTitle, link }) => {
 
     return (
         <section
-            className="relative h-[30dvh] md:h-[50dvh] w-full mt-80 xl:mt-80 px-7"
+            className="relative h-[30dvh] md:h-[50dvh] w-full mt-78 xl:mt-80 px-7"
             aria-label="Breadcrumb Navigation"
         >
             <Head>
